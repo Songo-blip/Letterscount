eb94bf775ef1614f799a554ec4e00e84227f36b7
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-10-16 15:49:40 +0200

    Add project files.

---
+++ Letterscount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Letterscount
+{
+    class Program
+    {
+
+        public static string CountCharactersInSentence(string inputString)
+        {
+
+            Char CharCount;
+            inputString = "Hello world";
+            int Count = 0;
+
+            CharCount = Convert.ToChar(Console.ReadLine());
+
+            foreach (char item in inputString)
+            {
+                if (item == CharCount)
+                {
+                    {
+                        Count++;
+                    }
+                }
+
+            }                      return (Count);
+
+
+               }
+
+        public static void Main()
+
+        {
+            string Words = "Hello world";
+
+
+            string CharCounts = Words.CountCharactersInSentence("Hello world");
+
+            
+            {
+                Console.WriteLine(CharCounts);
+            }
+        }
+    }
+}
+
